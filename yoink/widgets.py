44d30db30d87e13459e9341d5bf00a9b2bef8396
Matt Terry <terry10@llnl.gov>
2013-05-14 15:41:35 -0700

    add keyboard crop widget

--- yoink/widgets.py
+++ yoink/widgets.py
@@ -5,6 +5,7 @@ from matplotlib.patches import Circle, Rectangle
 from matplotlib.lines import Line2D
 from matplotlib.colors import LinearSegmentedColormap
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.pyplot as plt
 
 from yoink.trace import equispaced_colormaping
 
@@ -279,3 +280,63 @@ class CoordinateMapper(object):
     def __init__(self):
         pass
 
+
+class KeyboardCrop(object):
+    """
+    Keyboard driven interface to set crop an image
+
+    KeyboardCrop.edge_effects is a dict that controls the keyboard interface.
+    the keys are the edges effected (north/south/east/west)
+    the values are dicts of keyboard-keys and how far to shift the image
+    (nominally in pixels).
+
+    Example usage given in keyboard_crop()
+    """
+    def __init__(self, im, limits, width=20, height=20, **kwargs):
+        self.im = im
+        self.crop = limits
+        fig, axes = plt.subplots(2, 2, sharex='col', sharey='row')
+        self.canvas = fig.canvas
+        (self.ax1, self.ax2), (self.ax3, self.ax4) = axes
+        self.edge_effects = {}
+
+        self.width = width
+        self.height = height
+
+        self.ax1.imshow(im, **kwargs)
+        self.ax2.imshow(im, **kwargs)
+        self.ax3.imshow(im, **kwargs)
+        self.ax4.imshow(im, **kwargs)
+        self.update_limits()
+
+    def update_limits(self):
+        self.ax1.set_xlim(
+                self.crop['west'],     self.crop['west']+self.width)  # 1 3
+        self.ax2.set_xlim(
+                self.crop['east']-self.width,  self.crop['east'])     # 2 4
+        self.ax1.set_ylim(
+                self.crop['north'],    self.crop['north']+self.height)  # 1 2
+        self.ax3.set_ylim(
+                self.crop['south']-self.height, self.crop['south'])     # 3 4
+        self.canvas.draw()
+
+    def on_press(self, event):
+        key = event.key
+        if key is 'enter':
+            return
+
+        for edge, effects in self.edge_effects:
+            if key in effects:
+                self.crop[edge] += effects[key]
+        self.update_limits()
+
+    def connect(self):
+        self.press_cid = self.canvas.mpl_connect(
+                'key_press_event', self.on_press)
+
+    def disconnect(self):
+        self.canvas.mpl_disconnect(self.press_cid)
+        self.press_cid = None
+
+    def get_edges(self):
+        return [self.crop[key] for key in ('south', 'east', 'north', 'west')]
