afe1fcd825ccbb80fdafd358eaad8a1d351681bd
Matt Terry <terry10@llnl.gov>
2013-05-13 21:02:24 -0700

    moved contents of border.py to widgets.py

--- widgets.py
+++ widgets.py
@@ -1,5 +1,9 @@
+from __future__ import division
+
 from trace import equispaced_colormaping
 
+from matplotlib.patches import Circle
+from matplotlib.lines import Line2D
 from matplotlib.colors import LinearSegmentedColormap
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
@@ -48,3 +52,101 @@ class DragableCmap(object):
         assert None not in (self.l, self.rgb)
         seq = [(x, col) for x, col in zip(self.l, self.rgb)]
         return LinearSegmentedColormap.from_list(name, seq, **kwargs)
+
+
+class DeformableLine(object):
+    def __init__(self, ax, is_closed=False, max_points=None):
+        self.ax = ax
+        self.canvas = self.ax.figure.canvas
+
+        self.xs = []
+        self.ys = []
+        self.line = Line2D(self.xs, self.ys)
+        self.ax.add_artist(self.line)
+
+        self.circles = []
+
+        self.is_closed = is_closed
+        self.max_points = max_points 
+
+        self.callbacks = []
+        self.moving_ci = None
+        self.connected = False
+
+    def connect(self):
+        self.cidpress = self.canvas.mpl_connect(
+                'button_press_event', self.on_press)
+        self.cidrelease = self.canvas.mpl_connect(
+                'button_release_event', self.on_release)
+        self.cidmotion = self.canvas.mpl_connect(
+                'motion_notify_event', self.on_motion)
+        self.connected = True
+
+    def disconnect(self):
+        self.canvas.mpl_disconnect(self.cidpress)
+        self.canvas.mpl_disconnect(self.cidrelease)
+        self.canvas.mpl_disconnect(self.cidmotion)
+
+        self.cidpress = None
+        self.cidrelease = None
+        self.cidmotion = None
+        self.connected = False
+
+    def draw(self):
+        for f in self.callbacks:
+            f()
+        self.canvas.draw()
+
+    def on_press(self, event):
+        ci = self.get_circle_index(event)
+        if ci is None:
+            return
+        circle = self.circles[ci]
+        x0, y0 = circle.center
+        self.moving_ci = x0, y0, event.xdata, event.ydata, ci
+
+    def on_motion(self, event):
+        if self.moving_ci is None:
+            return
+        xc, yc, xclick, yclick, ci = self.moving_ci
+
+        x, y = xc + (event.xdata-xclick), yc + (event.ydata-yclick)
+        self.circles[ci].center = (x, y)
+
+        self.xs[ci], self.ys[ci] = x, y
+        if self.is_closed and len(self.circles) == self.max_points and ci == 0:
+            self.xs[-1], self.ys[-1] = x, y
+        self.line.set_data(self.xs, self.ys)
+
+        self.draw()
+
+    def on_release(self, event):
+        if self.moving_ci:
+            self.moving_ci = None
+            self.draw()
+
+    def get_circle_index(self, event):
+        i = 0
+        for i, c in enumerate(self.circles):
+            if c.contains(event)[0]:
+                return i
+        if len(self.circles) == self.max_points:
+            return None
+
+        # new circle
+        i = len(self.circles)
+        x, y = event.xdata, event.ydata
+        circle = Circle((x, y), radius=5, alpha=0.5)
+        self.circles.append(circle)
+        self.ax.add_artist(circle)
+
+        self.xs.append(x)
+        self.ys.append(y)
+        # finish square if adding last corner
+        if self.is_closed and len(self.circles) == self.max_points:
+            self.xs.append(self.xs[0])
+            self.ys.append(self.ys[0])
+        self.line.set_data(self.xs, self.ys)
+
+        self.draw()
+        return i
