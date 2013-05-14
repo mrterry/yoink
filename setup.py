44e8a4439ded187796f106e470f8908406214e4f
Matt Terry <terry10@llnl.gov>
2013-05-14 13:22:22 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='yoink',
+        version=0.1,
+        description='Yoinking data from rasterized images',
+        author='Matt Terry',
+        author_email='matt.terry@gmail.com',
+        url='https://github.com/mrterry/yoink',
+        packages=['yoink'],
+        )
