43dfd3f53713afc3a95f8520a807b0ad2f4ba8ac
Matt Terry <terry10@llnl.gov>
2013-06-11 19:43:44 -0700

    yoink_cmap is feature complete

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='yoink',
-    version=0.1,
+    version=0.2,
     description='Yoinking data from rasterized images',
     author='Matt Terry',
     author_email='matt.terry@gmail.com',
