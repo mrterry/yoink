08c5a112f62267616f6b16202203a4508547207b
Matt Terry <terry10@llnl.gov>
2013-06-15 21:04:17 -0700

    keep rgb

--- yoink/widgets.py
+++ yoink/widgets.py
@@ -580,6 +580,7 @@ class RecoloredWidget(AxesWidget):
         if l is self.l:
             return
         self.l = l
+        self.rgb = rgb
         self.pixels[:, :] = invert_cmap_kdtree(self._pixels, l, rgb)
         self.cmap = make_cmap(l, rgb)
         self.image.set_cmap(self.cmap)
