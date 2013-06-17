a544db068bdb7f327217712820e5a6f32b42114c
Matt Terry <terry10@llnl.gov>
2013-06-17 10:16:44 -0700

    remove unused CornerInterp

--- yoink/interp.py
+++ yoink/interp.py
@@ -3,37 +3,6 @@ from scipy.optimize import leastsq
 from scipy.spatial import cKDTree
 
 
-def CornerInterp(pixel_xy, data_xy):
-    """
-    Given pixel coordinates and equivalent data coordinates, return a function
-    that interpolates points in pixel space to points in data space.
-    """
-    A = np.zeros((len(pixel_xy), 3))
-    A[:, 1:] = pixel_xy
-    coefs, res, rank, s = np.linalg.lstsq(A, data_xy)
-    coefs = coefs.T
-
-    def f(px, py):
-        """
-        Converts points in pixel space (px, py) to points in data
-        space
-        """
-        # coerce px to array
-        if not hasattr(px, '__len__'):
-            px = [px]
-        if not hasattr(py, '__len__'):
-            py = [py]
-        n = len(px)
-
-        b = np.ones((3, n))
-        b[1, :] = px
-        b[2, :] = py
-        ans = np.dot(coefs, b)
-        return ans
-
-    return f
-
-
 def invert_cmap_leastsq(pix, l, colors):
     """
     Given a sequence of pixels, convert each to an equivalent index in the
