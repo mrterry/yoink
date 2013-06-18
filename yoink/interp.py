02dba6f364a86ded7d7bc8dd061693f54529bdd5
Matt Terry <terry10@llnl.gov>
2013-06-17 22:09:32 -0700

    remove slow invert_cmap_* variants

--- yoink/interp.py
+++ yoink/interp.py
@@ -1,59 +1,5 @@
 import numpy as np
-from scipy.optimize import leastsq
 from scipy.spatial import cKDTree
-
-
-def invert_cmap_leastsq(pix, l, colors):
-    """
-    Given a sequence of pixels, convert each to an equivalent index in the
-    color sequence l, colors
-
-    Performs a least squares minimization of the error to find the index.
-    color(f) is interpolated.  Error is quadrature sum of color erros.
-    """
-    pix = pix[..., :3]  # only want rgb
-    shape = pix.shape
-
-    ans = np.zeros(list(shape)[:-1], dtype=float)
-
-    def residuals(f, RGB):
-        return [np.interp(f, l, colors[:, i]) - RGB[i] for i in range(3)]
-
-    guess = 0.5
-    it = np.nditer(pix[..., 0], flags=['multi_index'])
-    while not it.finished:
-        index = it.multi_index
-        # use the answer from the previous iteration to guess the next answer
-        guess, stuff = leastsq(residuals, guess, args=(pix[index],))
-        ans[index] = guess
-        it.iternext()
-
-    return ans
-
-
-def invert_cmap_argmin(pix, l, colors):
-    """
-    Given a sequence of pixels, convert each to an equivalent index in the
-    color sequence l, colors
-
-    Finds the error between the target pixes and all colors in the cmap.
-    Picks the color with the smallest error.
-    Error is quadrature sum of color erros.
-    """
-    shape = pix.shape
-
-    ans = np.zeros(list(shape)[:-1], dtype=float)
-
-    it = np.nditer(pix[..., 0], flags=['multi_index'])
-    while not it.finished:
-        index = it.multi_index
-
-        err = colors - pix[index]
-        err = np.sum(err * err, axis=1)
-        ans[index] = l[err.argmin()]
-        it.iternext()
-
-    return ans
 
 
 def invert_cmap_kdtree(pix, l, colors):
