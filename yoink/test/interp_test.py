5039daa14b4938bb84d777a9a31d307e31fb2386
Matt Terry <terry10@llnl.gov>
2013-06-17 22:14:15 -0700

    add invert_cmap test

--- yoink/test/interp_test.py
+++ yoink/test/interp_test.py
@@ -1,7 +1,8 @@
 from itertools import permutations
+import numpy as np
 from nose.tools import ok_
 
-from yoink.interp import order_corners, get_corner_grid
+from yoink.interp import order_corners, get_corner_grid, invert_cmap
 
 
 def order_corners_test():
@@ -15,3 +16,17 @@ def get_corner_grid_test():
     ni, nj = 4, 5
     corners = [(1, 1), (ni-1, 1), (ni-1, nj-1), (1, nj-1)]
     get_corner_grid(corners, ni, nj)
+
+
+def invert_cmap_test():
+    k = 3
+    colors = np.ones((20, k))
+    l = np.linspace(0, 1, 20)
+    colors *= l[:, None]
+
+    ni, nj = 20, 20
+    pix = np.random.random((ni, nj, k))
+    z = invert_cmap(pix, l, colors)
+    assert z.min() >= l[0]
+    assert z.max() <= l[-1]
+    assert z.shape == (ni, nj)
