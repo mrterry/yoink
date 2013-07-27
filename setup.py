82edf960917754869ff2bb9fee7fc77d74c60aad
Matt Terry <matt.terry@gmail.com>
2013-07-27 16:11:01 -0700

    setup.py refactor, better PKG-DATA

--- setup.py
+++ setup.py
@@ -1,40 +1,52 @@
-from textwrap import dedent
-
-VERSION = '0.2dev'
-
-classifiers = dedent("""\
-    Development Status :: 3 - Alpha
-    Intended Audience :: Science/Research
-    License :: OSI Approved :: BSD License
-    Programming Language :: Python :: 2.7
-    Programming Language :: Python :: 3.3
-    Programming Language :: Python :: Implementation :: CPython
-    Topic :: Scientific/Engineering :: Visualization
-    Topic :: Utilities""")
-
-description = dedent("""\
-    Yoink is a tool for extracting data from rasterized images.  It currently
-    supports interactively picking points from line plots.  It also supports
-    extracting the scalar field from a colormapped image.  Yoink builds on top
-    matplotlib and is platform and rendering backend independent.""")
-
 from setuptools import setup
 
+descr = """Data Extraction Tools
+
+Yoink is a tool for extracting data from rasterized images.  It currently
+supports interactively picking points from line plots.  It also supports
+extracting the scalar field from a colormapped image.  Yoink builds on top
+matplotlib and is platform and rendering backend independent.
+
+Please refer to the online documentation at http://yoinkery.com/
+"""
+
+DISTNAME = 'yoinkery'
+DESCRIPTION = 'Yoinking data from rasterized images'
+LONG_DESCRIPTION = descr
+MAINTAINER = 'Matt Terry'
+MAINTAINER_EMAIL = 'matt.terry@gmail.com'
+URL = 'http://yoinkery.com'
+LICENSE = 'Modified BSD'
+VERSION = '0.2dev'
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Utilities',
+]
+
 setup(
-    name='yoinkery',
+    name=DISTNAME,
     version=VERSION,
     author='Matt Terry',
     author_email='matt.terry@gmail.com',
     home_page=None,
-    license='BSD',
-    summary='Yoinking data from rasterized images',
-    description=description,
+    license=LICENSE,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     platform='Any',
     hidden=True,
-    classifiers=classifiers,
-    url='https://github.com/mrterry/yoink',
+    classifiers=CLASSIFIERS,
+    url=URL,
     packages=['yoink'],
     package_data={'yoink': ['data/*.png']},
     scripts=['bin/yoink'],
     install_requires=['numpy', 'scipy', 'matplotlib >= 1.2'],
+    requires=['numpy', 'scipy', 'matplotlib'],
+    include_package_data=True,
 )
