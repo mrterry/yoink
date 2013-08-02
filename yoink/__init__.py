74b5e7eaaa3d0a14bace32514152d594297b751e
Matt Terry <matt.terry@gmail.com>
2013-08-02 12:26:02 -0700

    still learning to spell

--- yoink/__init__.py
+++ yoink/__init__.py
@@ -3,4 +3,4 @@ Yoink is a collection of tools for extracting data from rasterized images.
 """
 
 from .cmap_app import CmapExtractor
-from .line_app import LineExtrator
+from .line_app import LineExtractor
